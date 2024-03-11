271054808be7b8b3ac9dc88c9a8a41360135a00d
AC-devon <inbox@avaulto.com>
2024-03-11 13:18:21 +0200

    update file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ const tritonKey = process.env['tritonKey'];
 export const environment = {
   production: false,
   solanaEnv: 'mainnet-beta',
-  solanaCluster: 'https://mb-avaulto-cc28.mainnet.rpcpool.com/'+ tritonKey,
+  solanaCluster: 'https://mb-avaulto-cc28.mainnet.rpcpool.com/',//+ tritonKey,
   platformFeeCollector:'81QNHLve6e9N2fYNoLUnf6tfHWV8Uq4qWZkkuZ8sAfU1',
   platformATAbSOLFeeCollector:'BKSiuLdsNrj6oqQCgSSVXhfraK4WdrC8t7mnYWcMcM13',
 };
