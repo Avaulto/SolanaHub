a7dab03d973e6282eb8d2e8193945f8b8b5a5b1a
AC-devon <inbox@avaulto.com>
2024-03-26 00:16:26 +0200

    dao

--- src/app/services/util.service.ts
+++ src/app/services/util.service.ts
@@ -43,7 +43,8 @@ export class UtilService {
     private _localStorage: LocalStorageService
   ) {
   }
-  public serverlessAPI = location.hostname === "localhost" ? 'http://localhost:3000' : 'https://api.SolanaHub.app'
+  private  _subDomain = location.host.split(".")[0] === 'dev' ? 'dev-api' : 'api';
+  public serverlessAPI = location.hostname === "localhost" ? 'http://localhost:3000' : `https://${this._subDomain}.SolanaHub.app`
 
 
   public get RPC(): string{
