6345fa192a136c89b9ecdf20c70ae4d3d0356cb9
AC-devon <inbox@avaulto.com>
2024-03-26 00:16:42 +0200

    add env

--- src/app/services/util.service.ts
+++ src/app/services/util.service.ts
@@ -46,6 +46,7 @@ export class UtilService {
   private  _subDomain = location.host.split(".")[0] === 'dev' ? 'dev-api' : 'api';
   public serverlessAPI = location.hostname === "localhost" ? 'http://localhost:3000' : `https://${this._subDomain}.SolanaHub.app`
 
+  
 
   public get RPC(): string{
     const config = JSON.parse(this._localStorage.getData('RPC'))?.value || environment.solanaCluster
