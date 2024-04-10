2d35dc918bbf9d8d9c0551a7f7dde0d287da1bbe
AC-devon <inbox@avaulto.com>
2024-04-10 20:03:50 +0300

    fix bridge UI

--- src/app/pages/bridge/bridge.page.ts
+++ src/app/pages/bridge/bridge.page.ts
@@ -21,18 +21,18 @@ export class BridgePage implements OnInit {
   constructor() { }
   public widgetLoading: boolean = true
   async ionViewWillEnter() {
+
+  }
+  async ngOnInit() {
     console.log('page loaded');
 
     try {
       await this._importDeBridge()
       this.widgetLoading = false
-      deBridge.widget({"v":"1","element":"debridgeWidget","title":"","description":"","width":"485","height":"620","r":null,"supportedChains":"{\"inputChains\":{\"1\":\"all\",\"10\":\"all\",\"56\":\"all\",\"137\":\"all\",\"8453\":\"all\",\"42161\":\"all\",\"43114\":\"all\",\"59144\":\"all\",\"7565164\":\"all\",\"245022934\":\"all\"},\"outputChains\":{\"7565164\":[\"\",\"EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v\"]}}","inputChain":1,"outputChain":7565164,"inputCurrency":"","outputCurrency":"","address":"","showSwapTransfer":false,"amount":"","outputAmount":"","isAmountFromNotModifiable":true,"isAmountToNotModifiable":true,"lang":"en","mode":"deswap","isEnableCalldata":false,"styles":"eyJhcHBCYWNrZ3JvdW5kIjoiI2ZmZmZmZiIsImFwcEFjY2VudEJnIjoicmdiYSgyNDgsMjQ4LDI0OCwwKSIsImJhZGdlIjoicmdiYSgyNTUsMjU1LDI1NSwwKSIsImJvcmRlclJhZGl1cyI6OCwicHJpbWFyeSI6IiM5YjM2NzgiLCJzZWNvbmRhcnkiOiIjODA0ZmIzIiwic3VjY2VzcyI6IiMxN2IyNmEiLCJlcnJvciI6IiNmMDQ0MzgiLCJ3YXJuaW5nIjoiI2Y3OTAwOSIsImljb25Db2xvciI6IiM4MDRmYjMiLCJmb250Q29sb3IiOiIjMDAwMDAwIiwiZm9udENvbG9yQWNjZW50IjoiI2ZmYWFlMiIsImZvbnRGYW1pbHkiOiJJbnRlciIsInByaW1hcnlCdG5UZXh0IjoiI2ZmZmZmZiIsInNlY29uZGFyeUJ0bkJnIjoicmdiYSgyNTUsMjU1LDI1NSwwKSIsImxpZ2h0QnRuQmciOiIifQ==","theme":"light","isHideLogo":true,"logo":""})
-    } catch (error) {
+      deBridge.widget({"v":"1","element":"debridgeWidget","title":"","description":"","width":"485","height":"700","r":null,"supportedChains":"{\"inputChains\":{\"1\":\"all\",\"10\":\"all\",\"56\":\"all\",\"137\":\"all\",\"8453\":\"all\",\"42161\":\"all\",\"43114\":\"all\",\"59144\":\"all\",\"7565164\":\"all\",\"245022934\":\"all\"},\"outputChains\":{\"7565164\":\"all\"}}","inputChain":1,"outputChain":7565164,"inputCurrency":"","outputCurrency":"","address":"","showSwapTransfer":false,"amount":"","outputAmount":"","isAmountFromNotModifiable":false,"isAmountToNotModifiable":false,"lang":"en","mode":"deswap","isEnableCalldata":false,"styles":"eyJhcHBCYWNrZ3JvdW5kIjoiI2ZmZmZmZiIsImFwcEFjY2VudEJnIjoicmdiYSgyNDgsMjQ4LDI0OCwwKSIsImJvcmRlclJhZGl1cyI6OCwicHJpbWFyeSI6IiM5YjM2NzgiLCJzZWNvbmRhcnkiOiIjODA0ZmIzIiwic3VjY2VzcyI6IiMxN2IyNmEiLCJlcnJvciI6IiNmMDQ0MzgiLCJ3YXJuaW5nIjoiI2Y3OTAwOSIsImljb25Db2xvciI6IiM5YjM2NzgiLCJmb250Q29sb3IiOiIjMDAwMDAwIiwiZm9udEZhbWlseSI6IkludGVyIiwicHJpbWFyeUJ0bkJnIjoiIzliMzY3OCIsInByaW1hcnlCdG5UZXh0IjoiI2ZmZmZmZiIsInNlY29uZGFyeUJ0bkJnIjoicmdiYSgyNTUsMjU1LDI1NSwwKSIsImxpZ2h0QnRuQmciOiIiLCJsaWdodEJ0blRleHQiOiIjMDAwMDAwIn0=","theme":"light","isHideLogo":true,"logo":""})    } catch (error) {
       console.error(error);
 
     }
-  }
-  ngOnInit() {
   }
   private _importDeBridge = async () => {
 
