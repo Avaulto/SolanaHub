92459d9cb52209c7e8bd49275179bae53326a5bc
AC-devon <inbox@avaulto.com>
2024-07-01 17:41:48 +0300

    prep donate

--- src/app/shared/layouts/donate/popup/popup.component.ts
+++ src/app/shared/layouts/donate/popup/popup.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { IonText, IonLabel } from "@ionic/angular/standalone";
+import { IonText, IonLabel, IonButton, IonIcon } from "@ionic/angular/standalone";
+import { addIcons } from 'ionicons';
+import { heartHalfOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
   styleUrls: ['./popup.component.scss'],
   standalone: true,
-  imports:[IonText, IonLabel]
+  imports:[IonText, IonLabel,IonButton,IonIcon]
 })
 export class PopupComponent  implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({heartHalfOutline})
+   }
 
   ngOnInit() {}
-
+  showBlink = false;
+  
 }
