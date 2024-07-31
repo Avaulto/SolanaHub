c0ea7212e53859d1f5138d1a536c7dd00e2f512b
AC-devon <inbox@avaulto.com>
2024-07-31 16:51:13 +0300

    test

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -7,9 +7,9 @@ export const environment: EnvironmentConfig = {
   production: false,
   solanaEnv: 'mainnet-beta',
   solanaCluster: 'https://carole-l8ne8x-fast-mainnet.helius-rpc.com',
-  apiUrl: "https://api.SolanaHub.app",
+  apiUrl: "https://dev-api.SolanaHub.app",
   platformFeeCollector:'81QNHLve6e9N2fYNoLUnf6tfHWV8Uq4qWZkkuZ8sAfU1',
   platformATAbSOLFeeCollector:'BKSiuLdsNrj6oqQCgSSVXhfraK4WdrC8t7mnYWcMcM13',
-  turnStile: '0x4AAAAAAAVqd3Q0Le6TMHMl',
+  turnStile: '0x4AAAAAAAWLe07W81onsc2Q',
   ...getBaseConfig()
 };
