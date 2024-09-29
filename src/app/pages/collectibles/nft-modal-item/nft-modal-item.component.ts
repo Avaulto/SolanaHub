50eb77ab29ae7279d4e37e4390218596762895b1
AC-devon <inbox@avaulto.com>
2024-09-29 21:01:34 +0300

    remove zero value

--- src/app/pages/collectibles/nft-modal-item/nft-modal-item.component.ts
+++ src/app/pages/collectibles/nft-modal-item/nft-modal-item.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NFT } from 'src/app/models';
 import { IonImg, IonLabel, IonText } from "@ionic/angular/standalone";
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'nft-modal-item',
   templateUrl: './nft-modal-item.component.html',
   styleUrls: ['./nft-modal-item.component.scss'],
   standalone: true,
-  imports:[IonImg,IonLabel, IonText]
+  imports:[DecimalPipe,IonImg,IonLabel, IonText]
 })
 export class NftModalItemComponent  implements OnInit {
   @Input() nft:NFT = null
