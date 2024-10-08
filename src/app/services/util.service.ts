45a3975f68b305cf740eced773cc3a505c70d5a3
AC-devon <inbox@avaulto.com>
2024-10-09 00:44:17 +0300

    update t

--- src/app/services/util.service.ts
+++ src/app/services/util.service.ts
@@ -71,11 +71,11 @@ export class UtilService {
 
   public formatBigNumbers = (n: number) => {
     if (n < 1e3) return this.decimalPipe.transform(n, '1.2-2');
-    if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + "K";
-    if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
-    if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "B";
-    if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
-    else return n;
+    if (n >= 1e3 && n < 1e6) return Math.floor(n / 1e3 * 10) / 10 + "K";
+    if (n >= 1e6 && n < 1e9) return Math.floor(n / 1e6 * 10) / 10 + "M";
+    if (n >= 1e9 && n < 1e12) return Math.floor(n / 1e9 * 10) / 10 + "B";
+    if (n >= 1e12) return Math.floor(n / 1e12 * 10) / 10 + "T";
+    return n;
   };
 
 
