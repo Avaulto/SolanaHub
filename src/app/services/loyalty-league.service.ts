c1a090124c49144ba4c53ea2c7b8e1ae0c9c5354
AC-devon <inbox@avaulto.com>
2024-10-15 15:34:36 +0300

    new LL v2

--- src/app/services/loyalty-league.service.ts
+++ src/app/services/loyalty-league.service.ts
@@ -10,7 +10,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class LoyaltyLeagueService {
-  public hideLLv2 = signal(environment.production);
+  public hideLLv2 = signal(false);//environment.production);
   protected api = this._utilService.serverlessAPI + '/api/loyalty-league-v2'
   constructor(
     private _utilService: UtilService,
