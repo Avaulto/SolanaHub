ff51dd3681d83c2b68460f64a671f12cf90406fb
AC-devon <inbox@avaulto.com>
2024-10-24 17:00:17 +0300

    ui revmamp hubSOL page & jup fees

--- src/app/pages/hubsol/hubsol.page.ts
+++ src/app/pages/hubsol/hubsol.page.ts
@@ -1,11 +1,12 @@
-import { DecimalPipe, NgClass, PercentPipe } from '@angular/common';
+import { AsyncPipe, CurrencyPipe, DecimalPipe, NgClass, PercentPipe } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { IonButton, IonLabel, IonCol, IonImg, IonGrid, IonRow, IonContent, IonText, IonSkeletonText } from "@ionic/angular/standalone";
 import { Chart, ChartConfiguration, ChartItem } from 'chart.js';
 import Lottie from 'lottie-web';
+import { map, Observable, switchMap } from 'rxjs';
 
-import { UtilService } from 'src/app/services';
+import { ApiService, JupStoreService, UtilService } from 'src/app/services';
 interface LST_APY_HISTORY {
   epochAvg: number
   jupSOL: number
@@ -14,6 +15,11 @@ interface LST_APY_HISTORY {
   hubSOL: number
   currentEpoch: number
   date: string
+}
+interface HubSOLAdoption {
+  protocol: string
+  total: number
+  img?: string
 }
 
 @Component({
@@ -35,6 +41,8 @@ interface LST_APY_HISTORY {
     IonCol,
     IonLabel,
     IonButton,
+    AsyncPipe,
+    CurrencyPipe
   ]
 })
 export class HubsolPage implements OnInit, AfterViewInit {
@@ -44,43 +52,53 @@ export class HubsolPage implements OnInit, AfterViewInit {
   protected api = inject(UtilService).serverlessAPI + '/api'
   public supportedPlatforms = [
     {
+      protocol: 'phantom',
       img: 'assets/images/platforms/phantom.svg',
       type: 'wallet'
     },
     {
+      protocol: 'meteora',
       img: 'assets/images/platforms/meteora.svg',
       type: 'AMM'
     },
     {
+      protocol: 'jup',
       img: 'assets/images/platforms/jup.svg',
       type: 'aggregator'
     },
     {
+      protocol: 'orca',
       img: 'assets/images/platforms/orca.svg',
       type: 'DEX'
     },
     {
+      protocol: 'solayer',
       img: 'assets/images/platforms/solayer.svg',
       type: 'other'
     },
     {
+      protocol: 'solflare',
       img: 'assets/images/platforms/solflare.svg',
       type: 'wallet'
     },
     {
+      protocol: 'kamino',
       img: 'assets/images/platforms/kamino.svg',
       type: 'AMM'
     },
     {
+      protocol: 'raydium',
       img: 'assets/images/platforms/raydium.svg',
       type: 'AMM'
     },
     {
+      protocol: 'mango',
       img: 'assets/images/platforms/mango.svg',
       type: 'DEX'
     },
 
     {
+      protocol: 'texture',
       img: 'assets/images/platforms/texture.svg',
       type: 'AMM'
     },
@@ -94,7 +112,8 @@ export class HubsolPage implements OnInit, AfterViewInit {
     // }
   ]
   constructor(
-
+    private _apiService: ApiService,
+    private _jupService: JupStoreService
   ) { }
   ngOnInit() {
     this.startAnim()
@@ -154,7 +173,7 @@ export class HubsolPage implements OnInit, AfterViewInit {
     const config2: ChartConfiguration = {
       type: 'line',
       data: {
-        labels:  lstApyHistory.map(item =>  new Date(item.date).toLocaleDateString('en-US', { month: 'short', day: 'numeric' })), // X-axis labels
+        labels: lstApyHistory.map(item => new Date(item.date).toLocaleDateString('en-US', { month: 'short', day: 'numeric' })), // X-axis labels
         datasets: [
           {
             label: 'hubSOL',
@@ -224,7 +243,7 @@ export class HubsolPage implements OnInit, AfterViewInit {
             ticks: {
               align: 'inner',
               callback: function (val: any, index, dates) {
-              
+
                 return this.getLabelForValue(val);
               },
             },
@@ -248,31 +267,31 @@ export class HubsolPage implements OnInit, AfterViewInit {
           },
           tooltip: {
             enabled: true,
-          mode: 'index',
-          intersect: false,
-          position: 'nearest',
-          caretPadding: 10,
-          caretSize: 5,
-          cornerRadius: 4,
-          backgroundColor: 'rgba(0, 0, 0, 0.7)',
-          titleColor: 'white',
-          bodyColor: 'white',
-          // borderColor: 'rgba(255, 255, 255, 0.2)',
-          borderWidth: 1,
-          padding: 10,
-          displayColors: true,    
-          callbacks: {
-            label: function(context) {
-              let label = context.dataset.label || '';
-              if (label) {
-                label += ': ';
+            mode: 'index',
+            intersect: false,
+            position: 'nearest',
+            caretPadding: 10,
+            caretSize: 5,
+            cornerRadius: 4,
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
+            titleColor: 'white',
+            bodyColor: 'white',
+            // borderColor: 'rgba(255, 255, 255, 0.2)',
+            borderWidth: 1,
+            padding: 10,
+            displayColors: true,
+            callbacks: {
+              label: function (context) {
+                let label = context.dataset.label || '';
+                if (label) {
+                  label += ': ';
+                }
+                if (context.parsed.y !== null) {
+                  label += new DecimalPipe('en-US').transform(context.parsed.y * 100, '1.2-2') + '%';
+                }
+                return label;
               }
-              if (context.parsed.y !== null) {
-                label += new DecimalPipe('en-US').transform(context.parsed.y * 100, '1.2-2') + '%';
-              }
-              return label;
             }
-          }
           }
         }
       }
@@ -281,5 +300,101 @@ export class HubsolPage implements OnInit, AfterViewInit {
     this.chartData = new Chart(ctx, config2)
 
   }
+  public hubSOLAdoption: Observable<HubSOLAdoption[]> = this._apiService.get(this.api + '/hubSOL/get-holders?protocolsOnly=true')
+    .pipe(switchMap( async res => {
+      //   hubSOLBoosters: [
+      //     {
+      //       img: 'assets/images/ll/orca.svg',
+      //       title: 'orca',
+      //       pts: 0,
+      //       link: 'https://www.orca.so/pools?tokens=HUBsveNpjo5pWqNkH57QzxjQASdTVXcSK7bVKTSZtcSX',
+      //       badges: [{ strategy: 'Liquidity pools', protocolBoosted: true, solanahubboosted: false }]
+      //     },
+      //     {
+      //       img: 'assets/images/ll/kamino.svg',
+      //       title: 'kamino',
+      //       pts: 0,
+      //       link: 'https://app.kamino.finance/liquidity/7ycAn4vg4eZ82zeRWHey5qLQ53htEmeGq8CJ2tVXpPt9',
+      //       badges: [{ strategy: 'vault', protocolBoosted: false, solanahubboosted: false }]
 
+      //     },
+      //     {
+      //       img: 'assets/images/ll/yieldfan.webp',
+      //       title: 'yield.fan',
+      //       pts: 0,
+      //       link: 'https://yield.fan/dashboard',
+      //       badges: [{ strategy: 'Multiply', protocolBoosted: false, solanahubboosted: true }]
+      //     },
+      //     {
+      //       img: 'assets/images/ll/raydium.svg',
+      //       title: 'raydium',
+      //       pts: 0,
+      //       link: 'https://raydium.io/liquidity-pools/?token=HUBsveNpjo5pWqNkH57QzxjQASdTVXcSK7bVKTSZtcSX',
+      //       badges: [{ strategy: 'Liquidity pools', protocolBoosted: false, solanahubboosted: false }]
+
+      //     },
+      //     {
+      //       img: 'assets/images/ll/meteora.svg',
+      //       title: 'meteora',
+      //       pts: 0,
+      //       link: 'https://app.meteora.ag',
+      //       badges: [{ strategy: 'Liquidity pools', protocolBoosted: false, solanahubboosted: false }]
+
+      //     },
+      //     {
+      //       img: 'assets/images/ll/solayer.svg',
+      //       title: 'solayer',
+      //       pts: 0,
+      //       link: 'https://app.solayer.org/dashboard',
+      //       badges: [{ strategy: 'Restaking', protocolBoosted: false, solanahubboosted: false }]
+
+      //     },
+
+      //     {
+      //       img: 'assets/images/ll/texture.svg',
+      //       title: 'texture',
+      //       pts: 0,
+      //       link: 'https://texture.finance/lendy/lend',
+      //       badges: [{ strategy: 'P2P Lending', protocolBoosted: false, solanahubboosted: false }]
+      //     },
+
+      //     {
+      //       img: 'assets/images/ll/rainfi.svg',
+      //       title: 'rainfi',
+      //       pts: 0,
+      //       link: 'https://rain.fi/swap/hubSOL-SOL',
+      //       badges: [{ strategy: 'P2P Lending', protocolBoosted: false, solanahubboosted: false }]
+      //     },
+      //   ]
+      // }
+      const visibleList = [
+        { protocol: 'orca', img: 'assets/images/ll/orca.svg' },
+        { protocol: 'kamino', img: 'assets/images/ll/kamino.svg' },
+        { protocol: 'meteora', img: 'assets/images/ll/meteora.svg' },
+        { protocol: 'solayer', img: 'assets/images/ll/solayer.svg' },
+        { protocol: 'yield.fan', img: 'assets/images/ll/yieldfan.webp' }
+      ]
+      const hubSOL_price = await this._jupService.fetchPriceFeed('HUBsveNpjo5pWqNkH57QzxjQASdTVXcSK7bVKTSZtcSX')
+      const protocolsWithImg = res.map((item: HubSOLAdoption) => {
+        item.protocol = item.protocol === 'meteora_dlmm' ? 'meteora' : item.protocol
+        item.protocol = item.protocol === 'mango' ? 'yield.fan' : item.protocol
+
+        const itemWithImg = {
+          ...item,
+          total: item.total * hubSOL_price.data['HUBsveNpjo5pWqNkH57QzxjQASdTVXcSK7bVKTSZtcSX'].price,
+          img: visibleList.find(p => p.protocol === item.protocol)?.img
+        }
+        return itemWithImg
+      }).filter(item => item.total > 100 && item.protocol !== 'wallet')
+      return protocolsWithImg
+    }))
+  // private async _getLstApyHistory(): Promise<LST_APY_HISTORY[]> {
+  //   try {
+  //     const lstApyHistory = await (await fetch(`${this.api}/lst-apy`)).json()
+  //     return lstApyHistory
+  //   } catch (error) {
+  //     console.log(error);
+  //     return [] as any
+  //   }
+  // }
 }
