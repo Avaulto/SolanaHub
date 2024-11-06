202f138a1f7bbe69a6d7b334a4979fd1b1b27847
AC-devon <inbox@avaulto.com>
2024-11-06 17:25:23 +0200

    add loopscale

--- webpack.config.js
+++ webpack.config.js
@@ -31,8 +31,8 @@ module.exports = (config) => {
 			minimizer: [
 				new TerserPlugin({
 					terserOptions: {
-						keep_classnames: /.*Wallet.*|.*Adapter.*|.*Modal.*/,
-						keep_fnames: /.*Wallet.*|.*Adapter.*|.*Modal.*/,
+						keep_classnames: /.*Wallet.*|.*Adapter.*|.*ModalController.*/,
+						keep_fnames: /.*Wallet.*|.*Adapter.*|.*ModalController.*/,
 					},
 				}),
 			],
