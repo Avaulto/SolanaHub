1bc450428c000032aa4d4a4ea1f08cd13bd85eb6
AC-devon <inbox@avaulto.com>
2024-12-16 20:25:08 +0200

    filter failed route stash page

--- src/app/shared/components/news-feed/news-feed.component.ts
+++ src/app/shared/components/news-feed/news-feed.component.ts
@@ -25,13 +25,20 @@ export class NewsFeedComponent implements OnInit {
    }
 
   mainFeed = {
-    title: "Solana Foundation Announces New Grant Program",
+    title: "Stash page alive",
     type: "promotion",
     color: "active",
-    image: "https://pbs.twimg.com/media/GapFKSvWIAAFD0h?format=jpg&name=medium",
-    description: "for A limited time, stake hubSOL and earn up to 15% APY"
+    image: "https://pbs.twimg.com/media/Ge7aUTBXUAAOB8W?format=jpg&name=medium",
+    description: `Extract your SOL in 1 click <a href='https://discord.gg/bcVhnpww7N'>get it a try</a>`
   }
   feed = [
+    {
+      title: "hubSOL on kamino",
+      description: "hubSOL now available on Kamino multiply and lending product",
+      type: "DeFi",
+      color: "primary",
+      image: "assets/images/news-feed/news-1.png"
+    },
     {
       title: "Join Our Community",
       description: "Become a member of our <a href='https://discord.gg/bcVhnpww7N' target='_blank'>Discord Community</a> to stay updated with the latest news and announcements and get custom role",
@@ -51,13 +58,6 @@ export class NewsFeedComponent implements OnInit {
       description: "Our points system is now static and updated once per day",
       type: "informative",
       color: "focus",
-      image: "assets/images/news-feed/news-1.png"
-    },
-    {
-      title: "Full LST unstake support",
-      description: "We added full LST unstake support for all LST tokens on staking page",
-      type: "DeFi",
-      color: "primary",
       image: "assets/images/news-feed/news-1.png"
     },
     
