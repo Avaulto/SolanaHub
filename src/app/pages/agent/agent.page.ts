9174c9925ce17ab466096735e6de4603ed447938
AC-devon <inbox@avaulto.com>
2024-12-22 16:00:12 +0200

    p agent

--- src/app/pages/agent/agent.page.ts
+++ src/app/pages/agent/agent.page.ts
@@ -2,12 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { PageHeaderComponent } from 'src/app/shared/components/page-header/page-header.component';
 import { IonContent } from '@ionic/angular/standalone';
-import { SolanaAgentKit, createSolanaTools } from "solana-agent-kit";
-import { environment } from 'src/environments/environment';
-import { Keypair } from '@solana/web3.js';
-import { bs58 } from '@coral-xyz/anchor/dist/cjs/utils/bytes';
-import { SolanaBalanceTool, SolanaFetchPriceTool } from 'solana-agent-kit/dist/langchain';
-import { fetchPrice } from 'solana-agent-kit/dist/tools';
 
 import { PromoComponent } from './promo/promo.component';
 import { ChatBoxComponent } from './chat-box/chat-box.component';
